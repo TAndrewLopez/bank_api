0f67da68c2f2e2a16ae063a098238539fecae575
Andrew Dobson <tandrewlopez@gmail.com>
2024-03-03 17:10:50 -0500

    created simple logger

--- src/utils/index.utils.ts
+++ src/utils/index.utils.ts
@@ -1,15 +1,45 @@
 import { Response } from "express";
+import { createLogger, format, transports } from "winston";
 
 const printRed = (text: string) => {
     console.log("\x1b[31m%s\x1b[0m", `${text} \n`);
 };
 
-const handleError = (response: Response, message: string, statusCode: number = 400) => {
+const logger = createLogger({
+    transports: [
+        new transports.File({
+            filename: "./logs/index.log",
+            level: "error",
+            format: format.combine(
+                format.timestamp({
+                    format: "YYYY-MM-DD HH:mm:ss",
+                }),
+                format.printf(
+                    (info) => `${info.timestamp} ${info.level} : ${info.message}`
+                )
+            ),
+        }),
+    ],
+});
+
+const handleError = (
+    response: Response,
+    message: string,
+    statusCode: number = 400
+) => {
+    logger.log({ level: "error", message });
     return response.status(statusCode).json({ status: false, message });
 };
 
-const handleSuccess = (response: Response, message: string, data = {}, statusCode: number = 200) => {
-    return response.status(statusCode).json({ status: true, message, data: { ...data } });
+const handleSuccess = (
+    response: Response,
+    message: string,
+    data = {},
+    statusCode: number = 200
+) => {
+    return response
+        .status(statusCode)
+        .json({ status: true, message, data: { ...data } });
 };
 
 const generateCode = (num: number = 15) => {
